194618397958163b01473366f001691d1ddbac98
Adur Marques <adur.marques@gmail.com>
2023-12-27 10:58:55 +0100

    PNPM and packages
    npm has remplaced to pnpm and packages has been updated

--- src/theme/index.tsx
+++ src/theme/index.tsx
@@ -1,5 +1,4 @@
 import { useMemo, ReactNode } from 'react'
-import PropTypes from 'prop-types'
 
 import CssBaseline from '@mui/material/CssBaseline'
 import { createTheme, ThemeProvider as MUIThemeProvider } from '@mui/material/styles'
@@ -44,8 +43,4 @@ export default function ThemeProvider({ children }: ThemeProviderProps) {
       {children}
     </MUIThemeProvider>
   )
-}
-
-ThemeProvider.propTypes = {
-  children: PropTypes.node,
 }
